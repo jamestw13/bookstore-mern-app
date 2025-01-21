a31fd989cc6582e58dd5da64d3a3e6c12e148672
TJ James <jamestw13@gmail.com>
2025-01-21 10:50:37 -0600

    fixed tailwind not rendering issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,13 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '##FFCE1A',
+        primary: '#FFCE1A',
         secondary: '#0D0842',
         blackBG: '#F3F3F3',
         Favorite: '#FF5841',
       },
       fontFamily: {
-        primary: ['Montsserrat', 'sans-serif'],
+        primary: ['Montserrat', 'sans-serif'],
         secondary: ['Nunito Sans', 'sans-serif'],
       },
     },
