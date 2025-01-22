408992dd14facf1854d1b5a12dbc2ba37e0e2703
TJ James <jamestw13@gmail.com>
2025-01-22 08:54:15 -0600

    Added private routes

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -6,6 +6,7 @@ import Register from '../pages/login/Register';
 import Cart from '../pages/books/CartPage';
 import CheckoutPage from '../pages/books/CheckoutPage';
 import SingleBook from '../pages/books/SingleBook';
+import PrivateRoute from './PrivateRoute';
 
 export const router = createBrowserRouter([
   {
@@ -20,7 +21,14 @@ export const router = createBrowserRouter([
       { path: '/about', element: <h1>About</h1> },
       { path: '/contact', element: <h1>Contact</h1> },
       { path: '/cart', element: <Cart /> },
-      { path: '/checkout', element: <CheckoutPage /> },
+      {
+        path: '/checkout',
+        element: (
+          <PrivateRoute>
+            <CheckoutPage />
+          </PrivateRoute>
+        ),
+      },
       { path: '/dashboard', element: <h1>Dashboard</h1> },
       { path: '/login', element: <Login /> },
       { path: '/register', element: <Register /> },
